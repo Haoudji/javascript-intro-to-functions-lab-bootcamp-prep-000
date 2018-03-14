2e6b67b2ee524c139ec9521996409773c28641f8
Issa Boubacar <bacaris78@gmail.com>
2018-03-14 19:02:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,13 +13,13 @@ function logWhisper(string){
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string){
-  var uppercase = 'HELLO';
-  var lowercase = 'hello';
+  var uppercase = 'HELLO!';
+  var lowercase = 'hello!';
   var mixedCase = 'I love you, Grandma.';
   if(string === lowercase){
-    return "I can't hear you"
+    return ("I can't hear you").toLowerCase()
   }else if(string === uppercase){
-    return "YES INDEED!"
+    return ("YES INDEED!").toUpperCase()
   }else if(string === mixedCase){
     return "I love you, too"
   }
