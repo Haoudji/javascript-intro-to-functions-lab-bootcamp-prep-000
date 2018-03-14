8ed081909b20d4389e573e02165c1b38f77d30f0
Issa Boubacar <bacaris78@gmail.com>
2018-03-14 20:13:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string){
     return "I can't hear you!"
   }else if(string === string.toUpperCase()){
     return "YES INDEED!"
-  }else if(string === string.toLowerCase() && string === string.toUpperCase()){
+  }else if(string !== string.toLowerCase() && string !== string.toUpperCase()){
     return "I love you, too."
   }
   
