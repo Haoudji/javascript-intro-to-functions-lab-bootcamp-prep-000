07839b32b1067e3520a279be8c7a85a98a892da3
Issa Boubacar <bacaris78@gmail.com>
2018-03-14 19:18:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,11 +16,11 @@ function sayHiToGrandma(string){
   var uppercase = 'HELLO!';
   var lowercase = 'hello!';
   var mixedCase = 'I love you, Grandma.';
-  if(string === lowercase){
+  if(string === lowercase.toLowerCase()){
     return "I can't hear you"
-  }else if(string === uppercase){
+  }else if(string === uppercase.toUpperCase()){
     return ("YES INDEED!").toUpperCase()
-  }else if(string === mixedCase || string === mixedCase){
+  }else if(string === mixedCase.toLowerCase() || string === mixedCase.toUpperCase()){
     return "I love you, too"
   }
   
